6be35c385afcf70948f00511cd94e570d9d42086
myuon <ioi.joi.koi.loi@gmail.com>
2020-06-03 18:26:45 +0900

    feat: tui

---
+++ src/event.rs
@@ -0,0 +1,63 @@
+use std::io;
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    mpsc, Arc,
+};
+use std::thread;
+use std::time::Duration;
+use termion::event::Key;
+use termion::input::TermRead;
+
+pub enum Event {
+    Input(Key),
+    Tick,
+}
+
+pub struct Events {
+    rx: mpsc::Receiver<Event>,
+    input_handle: thread::JoinHandle<()>,
+    ignore_exit_key: Arc<AtomicBool>,
+    tick_handle: thread::JoinHandle<()>,
+}
+
+impl Events {
+    pub fn new(exit_key: Key) -> Self {
+        let (tx, rx) = mpsc::channel();
+        let ignore_exit_key = Arc::new(AtomicBool::new(false));
+        let input_handle = {
+            let tx = tx.clone();
+            let ignore_exit_key = ignore_exit_key.clone();
+            thread::spawn(move || {
+                let stdin = io::stdin();
+                for event in stdin.keys() {
+                    if let Ok(key) = event {
+                        if let Err(err) = tx.send(Event::Input(key)) {
+                            eprintln!("{}", err);
+                            return;
+                        }
+                        if !ignore_exit_key.load(Ordering::Relaxed) && key == exit_key {
+                            return;
+                        }
+                    }
+                }
+            })
+        };
+        let tick_handle = {
+            thread::spawn(move || loop {
+                tx.send(Event::Tick).unwrap();
+                thread::sleep(Duration::from_millis(250));
+            })
+        };
+
+        Events {
+            rx,
+            ignore_exit_key,
+            input_handle,
+            tick_handle,
+        }
+    }
+
+    pub fn next(&self) -> Result<Event, mpsc::RecvError> {
+        self.rx.recv()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io;
+use termion::event::Key;
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::style::{Color, Style};
+use tui::widgets::{Block, Borders, Clear, Paragraph, Text};
+use tui::Terminal;
+
+mod event;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    let events = event::Events::new(Key::Char('q'));
+
+    loop {
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .constraints(
+                    [
+                        Constraint::Length(f.size().height - 2),
+                        Constraint::Length(1),
+                        Constraint::Length(1),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+
+            let text = [Text::raw("This is a line \nPiyo piyo\n\nfooo")];
+            let paragraph = Paragraph::new(text.iter()).wrap(true);
+            f.render_widget(paragraph, chunks[0]);
+
+            let text = [Text::raw("= TXT EDIT =")];
+            let paragraph = Paragraph::new(text.iter())
+                .style(Style::default().bg(Color::White).fg(Color::Black))
+                .wrap(true);
+            f.render_widget(paragraph, chunks[1]);
+
+            f.render_widget(Paragraph::new([Text::raw("")].iter()), chunks[2]);
+        })?;
+
+        if let event::Event::Input(input) = events.next()? {
+            if let Key::Char('q') = input {
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
