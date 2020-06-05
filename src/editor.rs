2b58f5a0efe3696d5e70fd6ff601a027ed2d98e8
myuon <ioi.joi.koi.loi@gmail.com>
2020-06-05 10:22:02 +0900

    feat: cursor wrapping move

--- src/editor.rs
+++ src/editor.rs
@@ -41,19 +41,37 @@ impl Editor {
     }
 
     pub fn cursor_down(&mut self) {
-        if self.cursor.y < self.height - 1 {
+        if self.cursor.y < (self.height).min(self.text.len() as u16) {
             self.cursor.y += 1;
         }
     }
 
     pub fn cursor_left(&mut self) {
-        if self.cursor.x > 0 {
+        if self.cursor.x == 0 {
+            self.cursor_up();
+            self.cursor.x = self.text[self.cursor.y as usize].len() as u16 - 1;
+        } else if self.cursor.x > 0 {
             self.cursor.x -= 1;
         }
     }
 
     pub fn cursor_right(&mut self) {
-        if self.cursor.x < self.width - 1 {
+        let current_length = {
+            let current_line = self.text[self.cursor.y as usize]
+                .chars()
+                .collect::<Vec<_>>();
+
+            if current_line[current_line.len() - 1] == '\n' {
+                current_line.len() - 1
+            } else {
+                current_line.len()
+            }
+        } as u16;
+
+        if self.cursor.x == (self.width).min(current_length) {
+            self.cursor.x = 0;
+            self.cursor_down();
+        } else if self.cursor.x < (self.width).min(current_length) {
             self.cursor.x += 1;
         }
     }
